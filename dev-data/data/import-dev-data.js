da694fb77cd311e0132ecbb85ac100a9f31419dc
kevinbdx35 <k.bardoux@orange.fr>
2022-03-09 18:14:39 +0100

    fix bugs

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,18 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
-const dbName = 'punchline.db';
+const dbName = 'main.db';
 
 // Open the DB READ ONLY
-let db = new sqlite3.Database(dbName, sqlite3.OPEN_READONLY, (err) => {
+let db = new sqlite3.Database(dbName, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
-    return console.error('Database Error');
+    return console.error(err.message);
   }
   console.log('Database : ' + dbName + ' is ready.');
 });
 
-// close the DB
+// Data
+
+// Close the DB
 db.close((err) => {
   if (err) {
-    return console.error('Database Error');
+    return console.error(err.message);
   }
   console.log('Database : ' + dbName + ' is closed.');
 });
