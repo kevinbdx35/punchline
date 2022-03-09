fdd95e28b395407ba7cbccfe6ea6bd512f8956ea
kevinbdx35 <k.bardoux@orange.fr>
2022-03-09 16:50:52 +0100

    update

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -9,7 +9,7 @@ let db = new sqlite3.Database(dbName, sqlite3.OPEN_READONLY, (err) => {
   console.log('Database : ' + dbName + ' is ready.');
 });
 
-// clode the DB
+// close the DB
 db.close((err) => {
   if (err) {
     console.error('Database Errro');
