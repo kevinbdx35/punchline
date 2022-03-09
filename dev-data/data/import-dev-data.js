483a442c5f5c00e62fefe944f6da243cc51c580d
kevinbdx35 <k.bardoux@orange.fr>
2022-03-09 16:19:04 +0100

    add comment

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,13 +1,15 @@
 const sqlite3 = require('sqlite3');
 const dbName = 'punchline.db';
 
+// Open the DB READ ONLY
 let db = new sqlite3.Database(dbName, sqlite3.OPEN_READONLY, (err) => {
   if (err) {
-    console.error('Database Errro');
+    console.error('Database Error');
   }
   console.log('Database : ' + dbName + ' is ready.');
 });
 
+// clode the DB
 db.close((err) => {
   if (err) {
     console.error('Database Errro');
