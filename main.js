47b83b610defc7554b2f5d2de330107c66d0c868
kevinbdx35 <k.bardoux@orange.fr>
2022-03-12 17:59:58 +0100

    update the main.js

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 // Import
 const express = require('express');
+const quotesRouter = require('./routes/quotes');
 const path = require('path');
 const sass = require('sass');
 
 const app = express();
-
-const {
-    PORT = 3000,
-        NODE_ENV = 'development',
-} = process.env
+const port = 3000 || process.env.PORT;
 
 // Server configuration
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(__dirname + '/public'))
-app.use(express.static(__dirname + '/js_files'))
-app.listen(PORT, () => console.log('http://localhost:${PORT}'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/js_files'));
+
+app.listen(port, () => console.log(`start here : http://localhost:${port}`));
 
 // Routes configuration
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Punchline' })
-})
+  res.render('index', { title: 'Punchline' });
+});
