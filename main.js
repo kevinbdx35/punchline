8843242e837e14967d7f3e9ad8f2b645a0f45af8
kevinbdx35 <k.bardoux@orange.fr>
2022-03-12 19:55:48 +0100

    update package.json

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/js_files'));
 
+app.use(express.json());
+
 app.listen(port, () => console.log(`start here : http://localhost:${port}`));
 
 // Routes configuration
