840c64f9739799fbfd3c1d26c5bd04cfbece1382
DPGL <villainvictor4@gmail.com>
2022-03-16 11:55:28 +0100

    récupération de l'api en front terminer + ajout de la dépendence cors

--- main.js
+++ main.js
@@ -2,17 +2,12 @@
 const express = require('express');
 const quotesRouter = require('./routes/quotes');
 const path = require('path');
-const sass = require('sass');
+var cors = require('cors')
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
 app.use(express.json());
-
-// Routes configuration
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Punchline' });
-});
 
 // Server configuration
 app.set('view engine', 'ejs');
@@ -20,8 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/js_files'));
 
-app.use('/quotes', quotesRouter);
+app.use('/quotes', cors(), quotesRouter);
 
 app.listen(port, () => {
-  console.log(`start here : http://localhost:${port}`);
+    console.log(`start here : http://localhost:${port}`);
 });
