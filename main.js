686ca786921629c80837b9bce3c808757edd02a1
DPGL <villainvictor4@gmail.com>
2022-03-23 11:13:20 +0100

    bing bang deployed

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 var cors = require('cors')
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 
